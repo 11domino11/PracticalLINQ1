4e744e07546026ebedc55fdc9a91d50a6d8f6e52
Damion Russell <domino.russell@gmail.com>
2022-07-23 21:40:18 -0400

    initial commit

---
+++ PracticeLINQ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace PracticeLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> favoriteGames = new List<string>() {"sims","Animal Crossing", "Call of Duty", "Outward", "No man's sky"};
+            favoriteGames = favoriteGames.OrderBy(x => x.Length).ToList();
+            foreach (var item in favoriteGames)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
